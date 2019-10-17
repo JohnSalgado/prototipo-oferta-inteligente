90c19eb3c82fe55e86cad1cd444e5165c3b48fc6
JohnSalgado <devjohnsalgado@gmail.com>
2019-10-17 15:14:11 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Animação maquina de escrever
+
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split('')
+  elemento.innerHTML = ''
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => elemento.innerHTML += letra, 75 * i)
+  });
+}
+
+const titulo = document.querySelector('h3')
+typeWriter(titulo)
